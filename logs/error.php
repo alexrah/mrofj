f5238100b7c16047a820f4ed68762a1342d3836a
Ale Sandro <alexrah@gmail.com>
2014-07-01 01:57:53 +0200

    first commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-29 09:26:09 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-29T09:26:09+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-29T11:16:26+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ plugins/user/profile/profiles/validation.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) { 
+// Code using $ as usual goes here.
+document.formvalidator.setHandler('length11',
+     function (value) {
+             if (value.length == 11) {
+                 return true;
+             } else {
+                 return false;
+             }
+     }); 
+
+document.formvalidator.setHandler('condo',
+  function (value){
+    
+  });
+
+});
